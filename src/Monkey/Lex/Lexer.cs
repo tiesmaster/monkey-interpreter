1b9e50a59997ebbe695f785b15a447f7b14f57a3
Thijs Brobbel <thijsbrobbel@gmail.com>
2024-02-18 15:57:00 +0100

    Flesh out the first part of the lexer

--- src/Monkey/Lex/Lexer.cs
+++ src/Monkey/Lex/Lexer.cs
@@ -2,17 +2,51 @@
 
 namespace Monkey.Lex;
 
-public class Lexer(string input)
+public class Lexer
 {
-    private readonly string _input = input;
+    private readonly string _input;
+
+    private int _position;
+    private int _readPosition;
+    private byte _ch;
+
+    public Lexer(string input)
+    {
+        _input = input;
+
+        ReadChar();
+    }
 
     public Token NextToken()
     {
-        throw new NotImplementedException();
+        Token tok;
+        switch (_ch)
+        {
+            case (byte)'=':
+                tok = new(Tokens.Assign, "=");
+                break;
+            default:
+                tok = new(Tokens.Illegal, "ILLEGAL");
+                break;
+        }
+
+        ReadChar();
+
+        return tok;
     }
 
-    public void ReadChar()
+    private void ReadChar()
     {
-        throw new NotImplementedException();
+        if (_readPosition >= _input.Length)
+        {
+            _ch = 0;
+        }
+        else
+        {
+            _ch = (byte)_input[_readPosition];
+        }
+
+        _position = _readPosition;
+        _readPosition++;
     }
 }
